75f0e72985c07398f2662a00766735c2a7a78f71
dcwk <dcwk@yandex.ru>
2022-04-02 19:33:25 +0300

    Added implementation of current repository methods

--- domain/customer/memory/memory.go
+++ domain/customer/memory/memory.go
@@ -1,9 +1,11 @@
 package memory
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/dcwk/ddd-go/aggregate"
+	"github.com/dcwk/ddd-go/domain/customer"
 	"github.com/google/uuid"
 )
 
@@ -13,5 +15,45 @@ type MemoryRepository struct {
 }
 
 func New() *MemoryRepository {
-	return &MemoryRepository{}
+	return &MemoryRepository{
+		customers: make(map[uuid.UUID]aggregate.Customer),
+	}
+}
+
+func (mr *MemoryRepository) Get(id uuid.UUID) (aggregate.Customer, error) {
+	if customer, ok := mr.customers[id]; ok {
+		return customer, nil
+	}
+
+	return aggregate.Customer{}, customer.ErrCustomerNotFound
+}
+
+func (mr *MemoryRepository) Add(c aggregate.Customer) error {
+	if mr.customers == nil {
+		mr.Lock()
+		mr.customers = make(map[uuid.UUID]aggregate.Customer)
+		mr.Unlock()
+	}
+
+	if _, ok := mr.customers[c.GetId()]; ok {
+		return fmt.Errorf("customer already exists: %w", customer.ErrFailedToAddCustomer)
+	}
+
+	mr.Lock()
+	mr.customers[c.GetId()] = c
+	mr.Unlock()
+
+	return nil
+}
+
+func (mr *MemoryRepository) Update(c aggregate.Customer) error {
+	if _, ok := mr.customers[c.GetId()]; !ok {
+		return fmt.Errorf("customer does not exists: %w", customer.ErrUpdatedCustomer)
+	}
+
+	mr.Lock()
+	mr.customers[c.GetId()] = c
+	mr.Unlock()
+
+	return nil
 }
