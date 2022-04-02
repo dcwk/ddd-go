b3ad0184da523099fe4251b21a4e1a7e74871423
Ruslan Golovizin <r.golovizin@original-group.ru>
2022-04-02 16:26:33 +0300

    Added factory method for customer

--- aggregate/customer.go
+++ aggregate/customer.go
@@ -1,9 +1,34 @@
 package aggregate
 
-import "github.com/dcwk/ddd-go/entity"
+import (
+	"errors"
+
+	"github.com/dcwk/ddd-go/entity"
+	"github.com/dcwk/ddd-go/valueobject"
+	"github.com/google/uuid"
+)
+
+var ErrInvalidPerson = errors.New("a customer has to have an valid person")
 
 type Customer struct {
 	person       *entity.Person
 	products     []*entity.Item
 	transactions []*valueobject.Transaction
+}
+
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]*valueobject.Transaction, 0),
+	}, nil
 }
